f5bf554bd360ddcf26c6b7547a8893df26ecb357
Ben Faucher <faucher.benp@gmail.com>
2018-03-12 21:12:12 -0400

    Sensor list working! But having problem switching casts now.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'c3/c3.min.css';
 import 'react-widgets/dist/css/react-widgets.css';
-import C3Chart from 'react-c3js';
+//import C3Chart from 'react-c3js';
+import c3 from 'c3';
 import Moment from 'moment';
 import momentLocalizer from 'react-widgets-moment';
 import simpleNumberLocalizer from 'react-widgets-simple-number';
@@ -19,31 +20,54 @@ class App extends Component {
   constructor(props) {
     super(props);
 
+    // initialize casts list
     let selectedCast = getDefaultCast();
     let casts = getCasts();
+
+    // initialize dates
     let endTime = new Date();
     let startTime = new Date();
-    let startDate = startTime.getDate();
-    startTime.setDate(startDate - 1);
+    let startMonth = startTime.getMonth();
+    startTime.setMonth(startMonth - 1);       // one month before today
+
+    // initialize selected sensors
+    let selectedSensors = [];
+    for(let i = 0; i < 16; i++) {
+      selectedSensors.push(true);
+    }
+
+    // load initial data
+    let startTimeString = Moment(startTime).format('YYYYMMDDHHmmss');
+    let endTimeString = Moment(endTime).format('YYYYMMDDHHmmss');
+    let getData = getDataFromApi(selectedCast, startTimeString, endTimeString);
+    let points = parseMessage(getData);
+    //console.log(getData);
 
     this.state = {
       selectedCast: selectedCast,
       casts: casts,
-      points: [],
+      points: points,
       startTime: startTime,
-      endTime: endTime
+      endTime: endTime,
+      selectedSensors: selectedSensors
     };
+
+    // Flag used for managing chart redraws
+    this.dataUpdate = false;
   }
 
-  loadData() {
+  /* Callback Functions */
+
+  loadNewData() {
+    // Set flag for chart to be redrawn
+    this.dataUpdate = true;
+
     let startTimeString = Moment(this.state.startTime).format('YYYYMMDDHHmmss');
     let endTimeString = Moment(this.state.endTime).format('YYYYMMDDHHmmss');
-
-    let getData = getDataFromApi(this.state.selectedCast, startTimeString, endTimeString);
-    console.log(getData);
+    let apiData = getDataFromApi(this.state.selectedCast, startTimeString, endTimeString);
 
     let newState = {
-      points: parseMessage(getData)
+      points: parseMessage(apiData)
     };
 
     this.setState(newState);
@@ -72,14 +96,80 @@ class App extends Component {
 
     this.setState(newState);
   }
-  
-  render() {
 
+  processSensorChange(e, i) {
+    let newSelectedSensors = this.state.selectedSensors;
+
+    if (e.target.checked) {
+      newSelectedSensors[i] = true;
+      this.chart.show('s'+(i+1).toString());
+    } else {
+      newSelectedSensors[i] = false;
+      this.chart.hide('s'+(i+1).toString());
+    }
+
+    let newState = {
+      selectedSensors: newSelectedSensors
+    };
+
+    this.setState(newState);
+  }
+
+  showAllSensors() {
+    this.chart.show();
+    this.toggleAllSensors(true);
+  }
+
+  hideAllSensors() {
+    this.chart.hide();
+    this.toggleAllSensors(false);
+  }
+
+  /* Internal Functions */
+
+  toggleAllSensors(value) {
+    let newSelectedSensors = this.state.selectedSensors.map(() => value);
+
+    let newState = {
+      selectedSensors: newSelectedSensors
+    };
+
+    this.setState(newState);
+  }
+
+  renderSensorList() {
+    let sensors = [];
+
+    for(let i = 0; i < 16; i++) {
+      let id = 'slist_s' + i.toString();
+      let label = 'Sensor ' + (i+1).toString();
+      let checked = this.state.selectedSensors[i];
+
+      sensors.push(<SensorListItem key={id} id={id} label={label} checked={checked} onChange={event => this.processSensorChange(event, i)} />);
+    }
+
+    return sensors;
+  }
+
+  getHideMatrix() {
+    let mtx = [];
+    for(let i = 0; i < this.state.selectedSensors.length; i++) {
+      if(!this.state.selectedSensors[i]) {
+        mtx.push('s'+(i+1).toString());
+      }
+    }
+
+    return mtx;
+  }
+
+  // Must be called after initial render
+  generateChart() {
     let data = {
       x: 'x',
       xFormat: '%Y%M%d%H%M%S',
       rows: this.state.points,
-      type: 'spline'
+      type: 'spline',
+      hide: this.getHideMatrix()
     };
 
     let axis = {
@@ -90,6 +180,33 @@ class App extends Component {
         }
       }
     };
+
+    let chartConfig = {
+      //bindto: document.getElementById('chart'),
+      data: data,
+      axis: axis
+    };
+
+    return c3.generate(chartConfig);
+  }
+
+  // Unloads then re-loads data. Only call when necessary.
+  updateChartData() {
+    this.chart.unload();
+
+    // Only load data if there is data to load.
+    // This ensures the chart doesn't show erroneous data.
+    if(this.state.points.length !== 0) {
+      this.chart.load({rows: this.state.points});
+    }
+  }
+  
+  render() {
+    // Only re-render chart when necessary
+    if (this.dataUpdate) {
+      this.updateChartData();
+      this.dataUpdate = false;
+    }
 
     return (
       <div className="container-fluid">
@@ -108,30 +225,49 @@ class App extends Component {
               <div className="card-header">
                 <div className="row">
                   <div className="col-6 align-self-center">Sensors</div>
-                  <div className="col-2"><button type="button" className="btn btn-outline-primary btn-sm">All</button></div>
-                  <div className="col-4"><button type="button" className="btn btn-outline-secondary btn-sm">None</button></div>
+                  <div className="btn-group" role="group" aria-label="Select all/none buttons">
+                    <button type="button" className="btn btn-outline-primary btn-sm" onClick={() => this.showAllSensors()}>All</button>
+                    <button type="button" className="btn btn-outline-secondary btn-sm" onClick={() => this.hideAllSensors()}>None</button>
+                  </div>
                 </div>
               </div>
               <div className="card-body">
+                {this.renderSensorList()}
               </div>
             </div>
           </div>
           <div className="col-9">
             <div className="row">
-              <C3Chart data={data} axis={axis}/>
+              <div id="chart"></div>
             </div>
             <div className="row align-items-center">
               <div className="col-1 text-right">From:</div>
-              <div className="col"><DateTimePicker value={this.state.startTime} max={this.state.endTime} onChange={startTime => this.processStartTime(startTime)}/></div>
+              <div className="col"><DateTimePicker dropUp value={this.state.startTime} max={this.state.endTime} onChange={startTime => this.processStartTime(startTime)}/></div>
               <div className="col-1 text-right">To:</div>
-              <div className="col"><DateTimePicker value={this.state.endTime} max={new Date()} min={this.state.startTime} onChange={endTime => this.processEndTime(endTime)} /></div>
-              <div className="col-1"><button type="button" className="btn btn-outline-primary" onClick={() => this.loadData()}>Refresh</button></div>
+              <div className="col"><DateTimePicker dropUp value={this.state.endTime} max={new Date()} min={this.state.startTime} onChange={endTime => this.processEndTime(endTime)} /></div>
+              <div className="col-1"><button type="button" className="btn btn-outline-primary" onClick={() => this.loadNewData()}>Refresh</button></div>
             </div>
           </div>
         </div>
       </div>
     );
   }
+
+  componentDidMount() {
+    this.chart = this.generateChart();
+  }
+
+  // Only thing need to worry about is destroying chart
+  componentWillUnmount() {
+    if(this.chart) {
+      try {
+        this.chart = this.chart.destroy();
+      } catch (err) {
+        throw new Error('Internal C3 error', err);
+      }
+    }
+  }
+
 }
 
 export default App;
@@ -150,6 +286,15 @@ function CastSelectMenu(props) {
   );
 }
 
+function SensorListItem(props) {
+  return (
+    <div className="form-check">
+      <input className="form-check-input" type="checkbox" value="" id={props.id} onChange={props.onChange} checked={props.checked ? 'checked' : false}/>
+      <label className="form-check-label" htmlFor={props.id}>{props.label}</label>
+    </div>
+  );
+}
+
 function getCasts() {
   let casts = ['Cast1','FAUB_AR1'];
   return casts;
@@ -161,7 +306,7 @@ function getDefaultCast() {
 }
 
 function parseMessage(data) {
-  let vals = [['x','s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s16']];
+  let vals = [];
   
   // assembling an array that looks like:
   //   [
@@ -173,9 +318,12 @@ function parseMessage(data) {
   //      .
   //   ]
   
-  data.forEach(function(ct) {
-    vals.push([ct.t].concat(ct.v));
-  });
+  if(data.length !== 0) {
+    vals.push(['x','s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11','s12','s13','s14','s15','s16']);
+    data.forEach(function(ct) {
+      vals.push([ct.t].concat(ct.v));
+    });
+  }
   
   return vals;
 }
